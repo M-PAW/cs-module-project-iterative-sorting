565ae691dc70be04a76e7b8939bd4bbfe64f7cf0
Michael Walter <michael@MacBook-Pro.local>
2020-06-16 09:11:57 -0600

    Completed iterative_sort.py and searchig.py, all tests pass. Imporved and correct version of binary search implemented.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(0, (len(arr))):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,17 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
+    first = 0
+    last = len(arr) - 1
+    middle = 0
+    
+    while first <= last:
+        middle = (last - first) // 2
+        if arr[middle] == target:
+            return middle
+        elif target > middle:
+            first = middle
+        else: 
+            last = middle
 
     return -1  # not found
